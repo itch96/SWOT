6af9c3a1d4310c71442aed2a443fe3638bb67ecb
Rahul Gupta <assitch96@gmail.com>
2016-06-12 19:49:32 +0530

    Uploaded all the files.

---
+++ jscript.js
@@ -0,0 +1,32 @@
+var main = function(){
+	$('div').on('click', function(){
+		$(this).toggleClass('col-xs-8 col-xs-offset-1 active');
+		$('body').addClass('bactive');
+		$('body').animate({scrollTop: $(this).offset().top}, 300);
+	});
+	$('.strength').on('click', function(){
+		document.getElementById('str').disabled = false;
+	});
+	$('.weakness').on('click', function(){
+		document.getElementById('wea').disabled = false;
+	});
+	$('.opportunity').on('click', function(){
+		document.getElementById('opp').disabled = false;
+	});
+	$('.threat').on('click', function(){
+		document.getElementById('thr').disabled = false;
+	});
+	$(document).keyup(function(e){
+		if(e.keyCode == 27){
+			$('div').removeClass('col-xs-offset-1 col-xs-8 active');
+			$('div').addClass('col-xs-6');
+			$('body').removeClass('bactive');
+			document.getElementById('str').disabled = true;
+			document.getElementById('wea').disabled = true;
+			document.getElementById('opp').disabled = true;
+			document.getElementById('thr').disabled = true;
+		}
+	});
+};
+
+$(document).ready(main);
