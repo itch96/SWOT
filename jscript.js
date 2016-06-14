f8e89e3c6600bf68e806d9aceb9487045d008078
Rahul Gupta <assitch96@gmail.com>
2016-06-14 20:27:01 +0530

    Removed mouseleave property and added focus on the textarea upon clicking.

--- jscript.js
+++ jscript.js
@@ -1,20 +1,15 @@
 var main = function(){
 	
 	$('div').on('click', function(){
-		$(this).addClass('col-xs-12 container col-xs-offset-1 active');
-		$('body').addClass('bactive');
+		$(this).toggleClass('col-xs-12 container col-xs-offset-1 active');
+		$('body').toggleClass('bactive');
 		$('body').animate({scrollTop: $(this).offset().top}, 300);
-		$(this).addClass('margindiv');
-	});
-	$('div').on('mouseleave', function(){
-		$(this).removeClass('col-xs-12 container col-xs-offset-1 active');
-		$('body').removeClass('bactive');
-		$(this).removeClass('margindiv');
+		$(this).toggleClass('margindiv');
 	});
 
 	//For the press of esc key 
 	$(document).keyup(function(e){
-		if(e.keyCode == 27){
+		if(e.keyCode == 27 ){
 			$('div').removeClass('col-xs-12 container col-xs-offset-1 active');
 			$('body').removeClass('bactive');
 			$('div').removeClass('margindiv');
@@ -27,30 +22,19 @@ var main = function(){
 
 	$('.strength').on('click', function(){
 		document.getElementById('str').disabled = false;
+		document.getElementById('str').focus();
 	});
-	$('.strength').on('mouseleave', function(){
-		document.getElementById('str').disabled = true;
-	});
-
 	$('.weakness').on('click', function(){
 		document.getElementById('wea').disabled = false;
+		document.getElementById('wea').focus();
 	});
-	$('.weakness').on('mouseleave', function(){
-		document.getElementById('wea').disabled = true;
-	});
-
 	$('.opportunity').on('click', function(){
 		document.getElementById('opp').disabled = false;
+		document.getElementById('opp').focus();
 	});
-	$('.opportunity').on('mouseleave', function(){
-		document.getElementById('opp').disabled = true;
-	});
-
 	$('.threat').on('click', function(){
 		document.getElementById('thr').disabled = false;
-	});
-	$('.threat').on('mouseleave', function(){
-		document.getElementById('thr').disabled = true;
+		document.getElementById('thr').focus();
 	});
 };
 
